c69c50b9f13aa36d7da605cb0b036f775480ab4b
werdillo <wediktivus@gmail.com>
2024-11-24 04:15:30 +0200

    relative links

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,14 @@ import { defineConfig } from 'astro/config';
 
 import solidJs from '@astrojs/solid-js';
 
+import relativeLinks from 'astro-relative-links';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  output: 'static',
+  trailingSlash: 'never',
+  build: {
+    format: 'directory',
+  },
+  integrations: [solidJs(), relativeLinks()]
 });
